14de741f06edca5876ec67c93186a5e571a93572
Artem Khrustal <artemkhrustal@gmail.com>
2021-05-05 15:02:38 +0300

    Added DAO and Model modules with test example

--- rest/src/main/java/com/dms/rest/HelloController.java
+++ rest/src/main/java/com/dms/rest/HelloController.java
@@ -1,12 +1,39 @@
 package com.dms.rest;
 
+import com.dms.dao.TutorialRepository;
+import com.dms.model.Tutorial;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class HelloController {
+
+    @Autowired
+    TutorialRepository tutorialRepository;
+
     @GetMapping("/hello")
     public String getHello() {
         return "Hello controller";
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<?> createTutorial(@RequestBody Tutorial tutorial) {
+        tutorialRepository.save(tutorial);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Tutorial>> getAll() {
+        List<Tutorial> tutorialList = new ArrayList<>(tutorialRepository.findAll());
+        return new ResponseEntity<>(tutorialList, HttpStatus.OK);
     }
 }
--- services/src/main/java/com/dms/services/ServicesApplication.java
+++ services/src/main/java/com/dms/services/ServicesApplication.java
@@ -2,10 +2,15 @@ package com.dms.services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.dms")
+@EnableJpaRepositories(basePackages = "com.dms")
+@EntityScan(basePackages = "com.dms.model")
 public class ServicesApplication {
 
     public static void main(String[] args) {
