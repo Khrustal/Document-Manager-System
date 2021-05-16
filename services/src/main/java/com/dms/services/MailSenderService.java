5670255202191a7783c2ba2b664bfb3e442f8815
Artem Khrustal <artemkhrustal@gmail.com>
2021-05-16 16:11:01 +0300

    Minor changes

--- services/src/main/java/com/dms/services/MailSenderService.java
+++ services/src/main/java/com/dms/services/MailSenderService.java
@@ -1,9 +1,6 @@
 package com.dms.services;
 
-import com.dms.model.Document;
-import com.dms.model.Storable;
 import com.dms.model.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
